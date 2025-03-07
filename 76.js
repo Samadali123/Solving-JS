941928e2da31a0786156ab28626615fbc359cb08
syed samad ali <samadali0125@gmail.com>
2025-03-07 15:23:20 +0530

    explain switch in js

---
+++ 76.js
@@ -0,0 +1,12 @@
+// switch case in js
+
+switch(false){
+      
+    case 12>3 : console.log("true chalaa")
+    break;
+
+    case 12<3 : console.log("false chalaa")
+    break;
+
+    default : console.log("default chalaaaa switch ka.")
+}
